7cbfe4ef091043059dc177c3edfbf10e02bcbe38
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2025-01-02 15:03:52 +0900

    Implement styled-components

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    // styledComponentsの有効化
+    styledComponents: true,
+  },
+}
+
+module.exports = nextConfig
---
+++ src/pages/_app.tsx
@@ -0,0 +1,51 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
+
+// グローバルのスタイル
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+textarea {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+* {
+ box-sizing: border-box;
+}
+
+a {
+  cursor: pointer;
+  text-decoration: none;
+  transition: .25s;
+  color: #000;
+}
+
+ol, ul {
+  list-style: none;
+}
+`
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewpport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
