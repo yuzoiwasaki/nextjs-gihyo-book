be35235ceebc3d057d7b0c811d705d50071d05a3
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2025-01-05 18:12:04 +0900

    Add storybook

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from './colors'
+import fontSizes from './fontSizes'
+import letterSpacings from './letterSpacings'
+import lineHeights from './lineHights'
+import space from './space'
+
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+  colors,
+} as const
