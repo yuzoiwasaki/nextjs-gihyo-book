f08b5581b5864259d929029d68598834b6ceba28
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2025-01-11 12:47:54 +0900

    Implement API Client

---
+++ src/types/data.d.ts
@@ -0,0 +1,32 @@
+// 商品のカテゴリ
+export type Category = 'shoes' | 'clothes' | 'book'
+// 商品の状態
+export type Condition = 'new' | 'used'
+
+// ユーザー
+export type User = {
+  id: number
+  username: string
+  displayName: string
+  email: string
+  profileImageUrl: string
+  description: string
+}
+
+// 商品
+export type Product = {
+  id: number
+  category: Category
+  title: string
+  description: string
+  imageUrl: string
+  blurDataUrl: string
+  price: number
+  condition: Condition
+  owner: User
+}
+
+// API Context
+export type ApiContext = {
+  apiRootUrl: string
+}
